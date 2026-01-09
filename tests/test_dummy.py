bc02fb2043e036f92c690bdbfc403f08a1082671
Natan Dias <30359003+natan-dias@users.noreply.github.com>
2026-01-09 17:09:22 +0000

    First workflow
    * First workflows
    
    * dummy-tests
    
    * rename test dummy

---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_addition(self):
+        result = 2 + 2
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
